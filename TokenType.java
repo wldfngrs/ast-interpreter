ca226868b8cfd6d9e76ef6a40e658fab3c120bbe
wldfngrs <wldfngrs@gmail.com>
2024-12-27 10:07:49 +0100

    initial commit

---
+++ TokenType.java
@@ -0,0 +1,12 @@
+package com.craftinginterpreters.lox;
+
+enum TokenType {
+  // Single-character tokens.
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+  // One or two character tokens.
+  BANG, BANG_EQUAL,
+  EQUAL, EQUAL_EQUAL,
+  GREATER, GREATER_EQUAL,
+}
